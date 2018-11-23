6698f357a1f0c3f8cad059f08e5f5a7c092dbc04
Flying Bear <Galka1999@gmail.com>
2018-11-23 19:58:12 +0300

    Fixed memory problems

--- project.py
+++ project.py
@@ -5,23 +5,19 @@ from string import ascii_letters as latin
 # reading tsv from archive into a one-sentence lemm \n separated file
 with lzma.open('ruwac-parsed.out.xz', mode='rt', encoding='utf-8') as f:
     with open('text.txt', mode='w', encoding='utf-8') as t:
-        text = ['']
         for line in f.readlines(10000):
             l = line.split('\t')
             if len(l) == 7:
                 lemm = l[3]
                 sent = l[1]
                 if sent == 'SENT':
-                    text.append('\n')
+                    print('\n', file=t, end='')
                 elif lemm not in punct:
-                    text.append(lemm)
-        print(*text, file=t)
+                    print(lemm, file=t, end=' ')
 
 # getting rid of sentences with latin characters
 with open('text.txt', mode='r', encoding='utf-8') as t:
     with open('clean.txt', mode='w', encoding='utf-8') as c:
-        text = ['']
         for line in t.readlines():
             if set(latin).intersection(set(line)) == set():
-                text.append(line.strip(' '))
-        print(*text, file=c, sep='')
+                print(line, file=c, end='')
