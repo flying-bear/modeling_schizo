8e1a5f8fdd945db2b974a69491d74e297173c5c6
Andrew Lloyd <galka1999@gmail.com>
2020-05-12 00:20:49 +0300

    PCA

--- r/correlation_pictures.r
+++ r/correlation_pictures.r
@@ -8,5 +8,7 @@ coh_all_measures[3:18] <- apply(coh_all_measures[3:18], 2, sc) # z-scale all mea
 
 res2 <- rcorr(as.matrix(coh_all_measures[3:18]))
 corrplot(res2$r, type="upper", order="alphabet", 
-                       p.mat = res2$P, sig.level = 0.05, insig = "blank",
-                       tl.col = "black", tl.cex = 0.75, method="number", number.cex = 0.65)
+          p.mat = res2$P, sig.level = 0.01, 
+         insig = "blank", tl.col = "black", 
+         tl.cex = 0.75, method="number", number.cex = 0.65,
+         title='Metrics correlation plot at significance level 0.01', mar=c(0,0,1.5,0))
